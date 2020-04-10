224e4b89745fcb72c2e9d8264b06f9610c89729d
Abdulgafaar Abioye <abdulgafaar.abioye@gmail.com>
2020-04-10 16:10:56 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import ResultComponent from "./src/components/ResultComponent"
+import KeyPadComponent from "./src/components/KeyPadComponent"
+
+
+class App extends Component  {
+    constrotor(props){
+        super()
+
+        this.state = {
+            result : ""
+        }
+
+    }
+    calculator = () =>{
+        this.setState({
+
+            result : (eval(this.state.result)|| "") + ""
+        })
+
+
+    }
+
+    reset = () => {
+        this.setState({
+            result : ""
+        })
+    }
+
+
+    backspace = () => {
+        try{
+            this.setState({
+            result : this.state.slice(0, -1)
+             })
+
+        }catch(e){
+            this.setState({
+                result: "error"
+            })
+        }
+        
+    }
+
+    render(){
+        return(
+            <div className="calculator-body">
+                <h1>A simple calculate...</h1>
+                <ResultComponent result={this.state.result}/>
+                <keyPayComponent onClick={this.onClick}/>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+export default App
---
+++ src/components/ResultComponent.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react'
+
+
+class extends ResultComponent{
+    constructor(){
+        super()
+       
+    }
+    render(){
+        return(
+            <div className="result">
+                <p>{result}</p>
+
+            </div>
+        )
+    }
+    
+}
+
+export default ResultComponent
